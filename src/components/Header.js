baecfb487d63b5524957d45a698717f83ed1aba3
Samantha Vale <karlasamantha@gmail.com>
2017-11-10 09:25:37 -0300

    First attempts with styled-components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
 import styled, {css} from 'styled-components';
 
+const Title = styled.h1`
+  font-size: 2.5em;
+  text-align: center;
+  text-transform: uppercase;
+  font-weight: 900;
+`;
+
+const Subtitle = styled.h2`
+  font-size: 1em;
+  text-align: center;
+`;
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: lightgray;
+  font-family: Roboto, sans-serif;
+`; 
+
 class Header extends Component {
   render() {
-    return (
-      <div>
-        <h1>Hello, i'm the header</h1>
-      </div>
+    return(
+      <Wrapper>
+        <Title>
+          Preciso de visto?
+        </Title>
+        <Subtitle>
+          Lista dos países que exigem visto 
+        </Subtitle>
+      </Wrapper>
     )
   }
 }
