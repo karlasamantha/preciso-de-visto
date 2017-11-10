0fd74c99b0ac2d8221cf8fea8c777f3ea24da75c
Samantha Vale <karlasamantha@gmail.com>
2017-11-10 07:37:35 -0300

    Adds two new components and saves styled components as dependency

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import styled, {css} from 'styled-components';
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, i'm the header</h1>
+      </div>
+    )
+  }
+}
+
+export default Header;
