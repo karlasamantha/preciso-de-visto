b8d5e1013b9d9ab4f8fa01db75524a551f6d1712
Samantha Vale <karlasamantha@gmail.com>
2017-11-24 19:58:12 -0300

    Removes color

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,7 +4,6 @@ import styled from 'styled-components';
 const Input = styled.input`
   padding: 0.5em;
   margin: 0.5em;
-  color: palevioletred;
   border-radius: 3px;
   border: 1px solid lightgray;
   width: 50%;
