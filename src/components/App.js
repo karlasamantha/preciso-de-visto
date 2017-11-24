aabd7475d6dfcb60187fa1de3d48817af3a27b11
Samantha Vale <karlasamantha@gmail.com>
2017-11-24 19:56:00 -0300

    Creates container component and imports it on app

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,17 @@ import './App.css';
 import Header from './Header';
 import SearchBar from './SearchBar';
 import List from './List';
+import Container from './Container';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
-        <SearchBar />
-        <List />
+        <Container>
+          <SearchBar />
+          <List />
+        </Container>
       </div>
     );
   }
